54e8f245e8944b34e501cf440b124d85bada3398
Arslene Kabinda <a.kabinda.ext@funecap.com>
2023-06-19 19:07:44 +0200

    feat: add termes page

--- pages/Termes.jsx
+++ pages/Termes.jsx
@@ -1,8 +1,50 @@
 
 export default function Termes () {
     return(
-        <div>
-            <h1>Termes et conditions</h1>
+        <div className="terme">
+            <h1>Conditions générales de jeu et de mise sur Scrabble Battle Money</h1>
+            <div>
+                <h3>Les conditions</h3>
+                <p>  Ces conditions générales régissent votre utilisation du jeu en ligne Scrabble Battle Money ainsi que toutes les mises et gains associés.</p>
+            </div>
+            <div>
+                <h3> Definitions</h3>
+                <ul>
+                    <li> <strong>Scrabble Battle Money</strong>: se réfère au jeu en ligne de Scrabble que vous trouverez sur le site Web et/ou sur la plateforme mobile de notre jeu.</li>
+                    <li> <strong>Partie</strong>: se réfère à un jeu de Scrabble entre deux joueurs basé sur les règles du jeu établies. Pour chaque partie, un des joueurs sera le «maître de la partie».</li>
+                    <li> <strong>Mise</strong>: se réfère à l’argent mis en jeu par les joueurs pour chaque partie.</li>
+                    <li> <strong>Gain</strong>: se réfère à l’argent que le gagnant peut retirer après avoir remporté une partie.</li>
+                </ul>
+               
+            </div>
+            <div>
+                <h3>Mises et gain</h3>
+                <p>Tout en jouant à Scrabble Battle Money vous et un autre joueur pouvez parier sur qui sera le gagnant. Celui qui parie et gagne sera remboursé de la mise. Les mises et gains seront assujettis aux limites et restrictions établies.</p>
+            </div>
+            <div>
+                <h3> Inscription et admittance</h3>
+                <p>Pour jouer à Scrabble Battle Money, vous devez vous inscrire sur le site via une plateforme mobile ou Web. Une fois inscrit, vous devez payer pour le montant que vous souhaitez parier. Seuls ceux qui ont atteint l’âge légal de 18 ans dans leur pays sont autorisés à jouer à Scrabble Battle Money et à parier de l’argent. </p>
+            </div>
+            <div>
+                <h3>Utilisation des fonds</h3>
+                <p>Les fonds obtenus avec une mise ne peuvent être utilisés que pour jouer à Scrabble Battle Money. Il n’est pas autorisé d’utiliser ces fonds pour toute autre chose que la participation à des parties.</p>
+            </div>
+            <div>
+                <h3> Conditions d’annulation et de remboursement</h3>
+                <p>Si une partie est annulée, le montant de la mise sera remboursé aux joueurs. Si une partie est interrompue, le maître de la partie décidera de l’issue de la partie et du partage des gains</p>
+            </div>
+            <div>
+                <h3> Responsabilités</h3>
+                <p>Les joueurs sont responsables des stratégies et des décisions qu’ils prennent durant le jeu. L’utilisation de logiciels tiers, d’outils ou de programmes qui peuvent influencer la partie est interdite. 8. Modification des Conditions Générales Scrabble Battle Money se réserve le droit de modifier ces conditions générales à tout moment. Toute modification sera affichée sur le site et/ou sur la plateforme mobile de notre jeu.</p>
+            </div>
+            <div>
+                <h3>Contact Développeurs</h3>
+                <div className="liens">
+                    <a href="arskabinda@gmail.com">arskabinda@gmail.com</a>
+                    <a href="paskasonga@gmail.com">paskasonga@gmail.com</a>
+                    <a href="kevanimakasa2019@gmail.com">kevanimakasa2019@gmail.com</a>
+                </div>
+            </div>
         </div>
     )
 }
